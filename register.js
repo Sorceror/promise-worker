e8c460f6bb0c7f40e3f6578eef259950d2eb8ae6
Tiger Oakes <tigeroakes@gmail.com>
2016-06-01 10:21:03 -0700

    Errors pass their message instead, and moved callback check back so the messageID can be passed

--- register.js
+++ register.js
@@ -7,32 +7,33 @@
  * @listens Worker~message 
  */
 function register(callback) {
-  if (typeof callback !== 'function') {
-      postOutgoingMessage(messageId, new Error(
-        'Please pass a function into register().'));}
+	function postOutgoingMessage(messageId, error, result) {
+		if (error && self.console && console.error) {
+			// This is to make errors easier to debug. I think it's important
+			// enough to just leave here without giving the user an option
+			// to silence it.
+			console.error('Worker caught an error:', error);
+		}
+		self.postMessage(JSON.stringify([messageId, error, result]))
+	}
 
-  function postOutgoingMessage(messageId, error, result) {
-    if (error && self.console && console.error) {
-      // This is to make errors easier to debug. I think it's important
-      // enough to just leave here without giving the user an option
-      // to silence it.
-      console.error('Worker caught an error:', error);
-    } 
-    self.postMessage(JSON.stringify([messageId, error, result]))
-  }
-
-  self.addEventListener('message', e => {
-    let [messageId, message] = JSON.parse(e.data);
-    Promise.resolve().then(() => callback(message))
-      .catch(error => {
-        postOutgoingMessage(messageId, error);
-      })
-      .then(finalResult => {
-        postOutgoingMessage(messageId, null, finalResult);
-      }).catch(finalError => {
-        postOutgoingMessage(messageId, finalError);
-      })
-  });
+	self.addEventListener('message', e => {
+		let [messageId, message] = JSON.parse(e.data);
+		
+		if (typeof callback !== 'function') {
+			postOutgoingMessage(messageId, 'Please pass a function into register().');
+		}
+		
+		Promise.resolve().then(() => callback(message))
+			.catch(error => {
+				postOutgoingMessage(messageId, error.message);
+			})
+			.then(finalResult => {
+				postOutgoingMessage(messageId, null, finalResult);
+			}).catch(finalError => {
+				postOutgoingMessage(messageId, finalError.message);
+			})
+	});
 }
 
 export default { register };
